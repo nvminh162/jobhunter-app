3992189fed34aad0fa0dbf68ad35278ef250c6fd
nvminh162 <nvminh162@gmail.com>
2025-08-07 23:16:26 +0700

    Initialize Jobhunter Backend Project with Gradle Wrapper and Spring Boot
    - Added Gradle wrapper files (gradle-wrapper.properties, gradlew, gradlew.bat) for project build management.
    - Created settings.gradle.kts to define the root project name as "jobhunter".
    - Implemented main application class (JobhunterBackendApplication.java) with Spring Boot annotations.
    - Configured application properties (application.properties) to set the application name.
    - Added a test class (JobhunterBackendApplicationTests.java) to verify the application context loads correctly.

---
+++ jobhunter-backend/src/test/java/com/nvminh162/jobhunter/JobhunterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvminh162.jobhunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobhunterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
