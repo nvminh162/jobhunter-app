4545821141bd9c96d9aa5304e22a0f3267a20c62
nvminh162 <nvminh162@gmail.com>
2025-08-17 23:26:24 +0700

    feat: Add pre-persist logic to Company entity for automatic timestamp and creator assignment

--- jobhunter-backend/src/main/java/com/nvminh162/jobhunter/domain/Company.java
+++ jobhunter-backend/src/main/java/com/nvminh162/jobhunter/domain/Company.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -35,4 +36,10 @@ public class Company {
     private Instant updatedAt;
     private String createdBy;
     private String updatedBy;
+
+    @PrePersist
+    public void handleBeforeCreate() {
+        this.createdBy = "nvminh162";
+        this.createdAt = Instant.now();
+    }
 }
