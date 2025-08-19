9ee334a37c061e44376c6fbe02f7718cf3cc8b4f
nvminh162 <nvminh162@gmail.com>
2025-08-19 23:21:44 +0700

    feat: Refactor User class to use Lombok for getters and setters, and add GenderEnum for gender representation

--- jobhunter-backend/src/main/java/com/nvminh162/jobhunter/domain/User.java
+++ jobhunter-backend/src/main/java/com/nvminh162/jobhunter/domain/User.java
@@ -1,13 +1,23 @@
 package com.nvminh162.jobhunter.domain;
 
+import java.time.Instant;
+
+import com.nvminh162.jobhunter.util.enumerate.GenderEnum;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,46 +25,15 @@ public class User {
     private String name;
     private String email;
     private String password;
+    private int age;
 
-    public User() {
-    }
+    @Enumerated(EnumType.STRING)
+    private GenderEnum gender;
 
-    public User(Long id, String name, String email, String password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    private String address;
+    private String refreshToken;
+    private Instant createAt;
+    private Instant updateAt;
+    private String createBy;
+    private String updateBy;
 }
---
+++ jobhunter-backend/src/main/java/com/nvminh162/jobhunter/util/enumerate/GenderEnum.java
@@ -0,0 +1,6 @@
+package com.nvminh162.jobhunter.util.enumerate;
+
+
+public enum GenderEnum {
+    FEMALE, MALE, OTHER
+}
