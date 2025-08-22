b9828bb226a454f2668baa4038caca7672e1f5f4
nvminh162 <nvminh162@gmail.com>
2025-08-22 15:27:28 +0700

    feat: Establish relationship between Company and User entities

--- jobhunter-backend/src/main/java/com/nvminh162/jobhunter/domain/Company.java
+++ jobhunter-backend/src/main/java/com/nvminh162/jobhunter/domain/Company.java
@@ -1,14 +1,17 @@
 package com.nvminh162.jobhunter.domain;
 
 import java.time.Instant;
+import java.util.List;
 
 import com.nvminh162.jobhunter.util.SecurityUtil;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -43,6 +46,9 @@ public class Company {
     private Instant updatedAt;
     private String createdBy;
     private String updatedBy;
+
+    @OneToMany(mappedBy = "company", fetch = FetchType.LAZY)
+    private List<User> users;
 
     @PrePersist
     public void handleBeforeCreate() {
--- jobhunter-backend/src/main/java/com/nvminh162/jobhunter/domain/User.java
+++ jobhunter-backend/src/main/java/com/nvminh162/jobhunter/domain/User.java
@@ -12,6 +12,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -45,11 +47,15 @@ public class User {
 
     @Column(columnDefinition = "MEDIUMTEXT")
     private String refreshToken;
-    
+
     private Instant createdAt;
     private Instant updatedAt;
     private String createdBy;
     private String updatedBy;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
 
     @PrePersist
     public void handleBeforeCreate() {
