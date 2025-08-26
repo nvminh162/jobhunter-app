abcf6613fc3794d06f62c1b614ad75737d3ddd80
nvminh162 <nvminh162@gmail.com>
2025-08-26 19:58:55 +0700

    feat: enhance email functionality with new sync methods and add job email template

--- jobhunter-backend/src/main/java/com/nvminh162/jobhunter/controller/EmailController.java
+++ jobhunter-backend/src/main/java/com/nvminh162/jobhunter/controller/EmailController.java
@@ -16,10 +16,31 @@ public class EmailController {
         this.emailService = emailService;
     }
 
-    @GetMapping("/email")
+    @GetMapping("/send-simple-email")
     @ApiMessage("Send test simple email")
     public String sendSimpleEmail() {
         emailService.sendSimpleEmail();
+        return "OK";
+    }
+
+    @GetMapping("/send-email-sync")
+    @ApiMessage("Send Email Sync")
+    public String sendEmailSync() {
+        emailService.sendEmailSync(
+            "nvminh162@gmail.com",
+            "Test send email",
+            "<h1><b>Hello</b></h1>",
+            false,
+            true
+        );
+        return "OK";
+    }
+
+    @GetMapping("/send-email-from-template-sync")
+    @ApiMessage("Send Email Sync")
+    public String sendEmailFromTemplateSync() {
+        emailService.sendEmailFromTemplateSync(
+            "nvminh162@gmail.com", "Test send email with template", "jobs"); //test in src\main\resources\templates\test.html
         return "OK";
     }
 }
